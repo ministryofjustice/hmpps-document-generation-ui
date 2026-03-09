1e7070125367fd3a4ed0dbc29ec291b065d361e1
bill-moj <bill.yeung@digital.justice.gov.uk>
2026-03-09 13:09:47 +0000

    Validate generate document variable input form

--- server/routes/download-document/routes.ts
+++ server/routes/download-document/routes.ts
@@ -2,13 +2,15 @@ import { Services } from '../../services'
 import { BaseRouter } from '../common/routes'
 import { DownloadDocumentController } from './controller'
 import { Page } from '../../services/auditService'
+import { validate } from '../../middleware/validation/validationMiddleware'
+import { schemaFactory } from './schema'
 
 export const DownloadDocumentRoutes = (services: Services) => {
   const { router, get, post } = BaseRouter()
   const controller = new DownloadDocumentController(services)
 
   get('/', Page.DOWNLOAD_DOCUMENT, controller.GET)
-  post('/', controller.POST)
+  post('/', validate(schemaFactory(services)), controller.POST)
 
   return router
 }
---
+++ server/routes/download-document/schema.ts
@@ -0,0 +1,25 @@
+import { z } from 'zod'
+import { Request, Response } from 'express'
+import { createSchema } from '../../middleware/validation/validationMiddleware'
+import { Services } from '../../services'
+
+export const schemaFactory =
+  ({ documentGenerationService }: Services) =>
+  async (req: Request, res: Response) => {
+    const props: { [key: string]: z.ZodTypeAny } = {
+      download: z.string().optional(),
+      reload: z.string().optional(),
+    }
+
+    const template = await documentGenerationService.getTemplateById({ res }, req.params['id'] as string)
+
+    for (const domain of template.variables.domains) {
+      for (const variable of domain.variables) {
+        props[variable.code] = z.string().regex(/^[a-zA-Z0-9\s.,'"\-/()&+#:@]*$/, {
+          message: `${variable.description} (${domain.description}) only accepts alphanumeric characters, space and the following symbols: . , ' " - / ( ) & + # : @`,
+        })
+      }
+    }
+
+    return createSchema(props)
+  }
