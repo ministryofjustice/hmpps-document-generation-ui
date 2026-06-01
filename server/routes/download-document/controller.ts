b822fd5bfe3b2ad6432d4f0401db293a765e9dd6
bill-moj <bill.yeung@digital.justice.gov.uk>
2026-06-01 14:28:51 +0100

    Fix download-document form response cache bug

--- server/routes/download-document/controller.ts
+++ server/routes/download-document/controller.ts
@@ -1,7 +1,6 @@
 import { Request, Response } from 'express'
 import { format } from 'date-fns'
 import { Services } from '../../services'
-import { FLASH_KEY__FORM_RESPONSES } from '../../utils/constants'
 import { GenerateDocumentQuery, getReadOnlyVariables, mapTemplateVariables } from './utils'
 import { permittedRedirect } from '../../utils/permittedRedirect'
 import logger from '../../../logger'
@@ -45,38 +44,33 @@ export class DownloadDocumentController {
     req: Request<{ id: string }, unknown, { [key: string]: string | undefined }, GenerateDocumentQuery>,
     res: Response,
   ) => {
-    req.flash(FLASH_KEY__FORM_RESPONSES, JSON.stringify(req.body))
-    if (req.body['reload'] !== undefined) {
-      res.redirect(req.originalUrl)
-    } else {
-      const template = await this.services.documentGenerationService.getTemplateById({ res }, req.params.id)
+    const template = await this.services.documentGenerationService.getTemplateById({ res }, req.params.id)
 
-      let image: { buffer: Buffer; originalname: string } | null = null
-      if (req.body['perImage']) {
-        try {
-          const buffer = await this.services.prisonApiService.getPrisonerImageAsBuffer({ res }, req.body['perImage'])
-          image = { buffer, originalname: `${req.body['perImage']}.png` }
-        } catch (error) {
-          logger.warn(error, `Unable to get image for Prison number ${req.body['perImage']}`)
-        }
+    let image: { buffer: Buffer; originalname: string } | null = null
+    if (req.body['perImage']) {
+      try {
+        const buffer = await this.services.prisonApiService.getPrisonerImageAsBuffer({ res }, req.body['perImage'])
+        image = { buffer, originalname: `${req.body['perImage']}.png` }
+      } catch (error) {
+        logger.warn(error, `Unable to get image for Prison number ${req.body['perImage']}`)
       }
-
-      let filename = template.code
-      if (req.query.prisonNumber) filename = `${filename}_${req.query.prisonNumber}`
-      filename = `${filename}_${res.locals.user.username}_${format(new Date(), 'yyyy-MM-dd_HH-mm-ss')}.docx`
-
-      const { perImage, _csrf, download, ...variables } = req.body
-
-      const document = await this.services.documentGenerationService.generateDocument(
-        { res },
-        req.params.id,
-        filename,
-        variables,
-        image,
-      )
-
-      res.setHeader('Content-Disposition', `attachment; filename="${filename}"`)
-      res.send(document)
     }
+
+    let filename = template.code
+    if (req.query.prisonNumber) filename = `${filename}_${req.query.prisonNumber}`
+    filename = `${filename}_${res.locals.user.username}_${format(new Date(), 'yyyy-MM-dd_HH-mm-ss')}.docx`
+
+    const { perImage, _csrf, download, ...variables } = req.body
+
+    const document = await this.services.documentGenerationService.generateDocument(
+      { res },
+      req.params.id,
+      filename,
+      variables,
+      image,
+    )
+
+    res.setHeader('Content-Disposition', `attachment; filename="${filename}"`)
+    res.send(document)
   }
 }
